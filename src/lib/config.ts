73df17ae877161827a8cf8bd209771f5be5ac964
Ahmed Fareed <ahmed@techchaps.com>
2024-12-22 01:29:55 +0000

    fix delete functionn on biz edit page

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'http://localhost:3000/api';
-export const API_BASE_URL = 'https://api.app.thrivehub.ai/api';
+export const API_BASE_URL = 'http://localhost:3000/api';
+// export const API_BASE_URL = 'https://api.app.thrivehub.ai/api';
